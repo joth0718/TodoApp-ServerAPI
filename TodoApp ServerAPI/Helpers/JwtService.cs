edf5d3cc10b153895afddc18cf70e77966e45e3d
joth0718 <joth0718@gmail.com>
2023-01-23 21:11:20 +0100

    Added: Jwt Service helper for future token, CORS for client/server communication and added Db, repositories, helper to program

---
+++ TodoApp ServerAPI/Helpers/JwtService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace TodoApp_ServerAPI.Helpers
+{
+    public class JwtService
+    {
+        private const string _secureKey = "secure key for jwt";
+
+        public string Generate(int id)
+        {
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secureKey));
+            var credentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+            var header = new JwtHeader(credentials);
+
+            var payLoad = new JwtPayload(id.ToString(), null, null, null, DateTime.Today.AddDays(1));
+            var securityToken = new JwtSecurityToken(header, payLoad);
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+
+
+        public JwtSecurityToken Verify(string jwt)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_secureKey);
+            tokenHandler.ValidateToken(jwt, new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = false,
+                ValidateAudience = false
+            }, out SecurityToken validatedToken);
+
+            return (JwtSecurityToken)validatedToken;
+        }
+    }
+}
--- TodoApp ServerAPI/Program.cs
+++ TodoApp ServerAPI/Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApp_ServerAPI.Data.Interfaces;
+using TodoApp_ServerAPI.Data;
+using TodoApp_ServerAPI.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy", builder =>
+    {
+        builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()
+            .WithOrigins("http://localhost:3000");
+    });
+});
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite("Data Source=./Data/AppDB.db"));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IItemRepository, ItemRepository>();
+builder.Services.AddScoped<JwtService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
